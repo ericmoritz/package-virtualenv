1890bd47f5db8f11748d1abfe77189076b4ef5af
Eric Moritz <emoritz@gannett.com>
2012-04-19 12:31:14 -0400

    Added a test for utils.find_files

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,9 @@
 import os
 from package_virtualenv import utils
 import unittest
+from itertools import imap
+import fixtures
+
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
@@ -8,14 +11,21 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 class TestFindLibraries(unittest.TestCase):
     
     def test(self):
-        root = os.path.join(HERE, "fixtures/test-env/")
-        
-        expected = set([os.path.join(root, filename)
-                    for filename in 
-                    ['lib/python2.6/site-packages/lxml/etree.so',
-                     'lib/python2.6/site-packages/lxml/objectify.so']
-                    ])
+        expected = set(imap(lambda fn: os.path.join(fixtures.VIRTUALENV, fn),
+                           ['lib/python2.6/site-packages/lxml/etree.so',
+                            'lib/python2.6/site-packages/lxml/objectify.so']))
 
-        result = set(utils.find_libraries(root))
+        result = set(utils.find_libraries(fixtures.VIRTUALENV))
+        self.assertEqual(expected, result)
+
+
+class TestFindFiles(unittest.TestCase):
+    
+    def test(self):
+        expected = set(imap(lambda fn: os.path.join(fixtures.VIRTUALENV, fn),
+                           fixtures.FILES))
+
+        result = set(utils.find_files(fixtures.VIRTUALENV))
 
         self.assertEqual(expected, result)
+
