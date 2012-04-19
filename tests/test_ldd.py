25b63d732600fd245d0c04ea0018f730653575d3
Eric Moritz <emoritz@gannett.com>
2012-04-19 12:30:46 -0400

    Used the fixtures module instead of hardcoding the fixtures

--- tests/test_ldd.py
+++ tests/test_ldd.py
@@ -1,57 +1,23 @@
 import unittest
 from package_virtualenv import ldd
 import os
-
-
-HERE = os.path.abspath(os.path.dirname(__file__))
+import fixtures
 
 
 class TestFindLinks(unittest.TestCase):
 
     def test(self):
-        fixture = os.path.join(HERE, "fixtures/test-env/lib/python2.6/site-packages/lxml/etree.so")
-
-        expected = set([
-            "/usr/lib64/libxslt.so.1",
-            "/usr/lib64/libexslt.so.0",
-            "/usr/lib64/libxml2.so.2",
-            "/lib64/libz.so.1",
-            "/lib64/libm.so.6",
-            "/usr/lib64/libpython2.6.so.1.0",
-            "/lib64/libpthread.so.0",
-            "/lib64/libc.so.6",
-            "/lib64/libgcrypt.so.11",
-            "/lib64/libdl.so.2",
-            "/lib64/libgpg-error.so.0",
-            "/lib64/libutil.so.1",
-            ])
-
+        fixture = os.path.join(fixtures.VIRTUALENV, "lib/python2.6/site-packages/lxml/etree.so")
+        expected = fixtures.LIBRARIES
         result = ldd.links(fixture)
-
         self.assertEqual(result, expected)
 
 
 class TestWalkLinks(unittest.TestCase):
     
     def test(self):
-        fixture = os.path.join(HERE, "fixtures/test-env/lib/python2.6/site-packages/lxml/etree.so")
-
-        expected = set([
-            "/usr/lib64/libxslt.so.1",
-            "/usr/lib64/libexslt.so.0",
-            "/usr/lib64/libxml2.so.2",
-            "/lib64/libz.so.1",
-            "/lib64/libm.so.6",
-            "/usr/lib64/libpython2.6.so.1.0",
-            "/lib64/libpthread.so.0",
-            "/lib64/libc.so.6",
-            "/lib64/libgcrypt.so.11",
-            "/lib64/libdl.so.2",
-            "/lib64/libgpg-error.so.0",
-            "/lib64/libutil.so.1",
-            ])
-
+        fixture = os.path.join(fixtures.VIRTUALENV, "lib/python2.6/site-packages/lxml/etree.so")
+        expected = fixtures.LIBRARIES
         result = ldd.walk_links(set(), fixture)
-
         self.assertEqual(expected, result)
         
