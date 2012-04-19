bfb39af7a0560f226a4c90e4583458c40ff68876
Eric Moritz <emoritz@gannett.com>
2012-04-19 12:27:23 -0400

    Updated the build_spec test

--- tests/test_rpm.py
+++ tests/test_rpm.py
@@ -2,42 +2,16 @@ import unittest
 import sys
 from package_virtualenv.backends import rpm
 import os
-
-
-HERE = os.path.abspath(os.path.dirname(__file__))
+from itertools import imap
+import fixtures
 
 
 class TestFindPackages(unittest.TestCase):
 
     def test(self):
-        lib_set = set([
-            "/usr/lib64/libxslt.so.1",
-            "/usr/lib64/libexslt.so.0",
-            "/usr/lib64/libxml2.so.2",
-            "/lib64/libz.so.1",
-            "/lib64/libm.so.6",
-            "/usr/lib64/libpython2.6.so.1.0",
-            "/lib64/libpthread.so.0",
-            "/lib64/libc.so.6",
-            "/lib64/libgcrypt.so.11",
-            "/lib64/libdl.so.2",
-            "/lib64/libgpg-error.so.0",
-            "/lib64/libutil.so.1",
-            ])
+        result = rpm.find_packages(fixtures.LIBRARIES)
+        expected = fixtures.PACKAGES
 
-
-        expected = set([
-                'libgpg-error-1.7-4.el6.x86_64',
-                'libxml2-2.7.6-4.el6_2.4.x86_64',
-                'python-libs-2.6.6-29.el6.x86_64',
-                'libgcrypt-1.4.5-9.el6_2.2.x86_64',
-                'glibc-2.12-1.47.el6_2.9.x86_64',
-                'zlib-1.2.3-27.el6.x86_64',
-                'libxslt-1.1.26-2.el6.x86_64'
-                ])
-
-        result = rpm.find_packages(lib_set)
-    
         self.assertEqual(expected, result)
 
 
@@ -45,19 +19,50 @@ class TestBuildSpec(unittest.TestCase):
     
     def test(self):
         expected = """Name: test-rpm
-Release: 1
 Summary: A test rpm
 Version: 1.0
+Release: 1
+License: BSD
+Requires: required-package, other-required-package
+
+%description
+A long description
+
+%build
+mkdir -p $RPM_BUILD_ROOT/opt/virtualenvs
+cp -r /tmp/test-env $RPM_BUILD_ROOT/opt/virtualenvs/
 
 %files
-/tmp/foo
+/opt/virtualenvs/test-env/foo
 """
 
-        result = rpm.build_spec({"Name": "test-rpm",
-                                 "Summary": "A test rpm",
-                                 "Version": "1.0",
-                                 "Release": "1"},
-                                files=["/tmp/foo"])
-        
+        result = rpm.build_spec("/tmp/test-env/",
+                                name="test-rpm",
+                                summary="A test rpm",
+                                description="A long description",
+                                version="1.0",
+                                release="1",
+                                license="BSD",
+                                files=["/tmp/test-env/foo"],
+                                requires=["required-package",
+                                          "other-required-package"])
+        print expected
+        print
+        print result
+
         self.assertEqual(expected, result)
+
         
+class TestCollectItems(unittest.TestCase):
+
+    def test(self):
+        def make_fixture(filename):
+            return os.path.join(fixtures.VIRTUALENV, filename)
+
+        expected = {"files": set(imap(make_fixture,
+                                 fixtures.FILES)),
+                    "packages": fixtures.PACKAGES}
+
+        result = rpm.collect_items(fixtures.VIRTUALENV)
+
+        self.assertEqual(expected, result)
